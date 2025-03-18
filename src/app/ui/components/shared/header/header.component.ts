06d1f36340de423bdb3b189fba5e39d3d6092915
ULPGCJuan <juanantonioher03@gmail.com>
2025-03-18 17:52:42 +0000

    feat: initial header

--- src/app/ui/components/shared/header/header.component.ts
+++ src/app/ui/components/shared/header/header.component.ts
@@ -1,18 +1,30 @@
-import {Component, Input} from '@angular/core';
-import {Router, RouterLink, RouterLinkActive} from '@angular/router';
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
+  standalone: true,
+  imports: [CommonModule]
 })
-export class HeaderComponent {
 
-  @Input() adminName: String = 'Juan Antonio'
+export class HeaderComponent {
+  @Input() adminName: string = 'Juan Antonio';
+  isDropdownOpen: boolean = false;
+
   constructor(private router: Router) {}
 
   navigateTo(path: string) {
     this.router.navigate([path]);
+  }
+
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  logout() {
+    //this.router.navigate(['/login']);
   }
 }
