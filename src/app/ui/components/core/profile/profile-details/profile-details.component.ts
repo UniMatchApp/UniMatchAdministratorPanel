9b1ca92cb9bdc06eec1c88f8860da8c12570ef1b
ULPGCJuan <juanantonioher03@gmail.com>
2025-03-25 17:24:07 +0000

    wip profile

---
+++ src/app/ui/components/core/profile/profile-details/profile-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-details',
+  imports: [],
+  templateUrl: './profile-details.component.html',
+  standalone: true,
+  styleUrl: './profile-details.component.css'
+})
+export class ProfileDetailsComponent {
+
+}
--- src/app/ui/screens/core/profile/profile.component.ts
+++ src/app/ui/screens/core/profile/profile.component.ts
@@ -1,42 +1,71 @@
 import {Component, OnInit} from '@angular/core';
 import {ReportsListComponent} from '../../../components/shared/reports-list/reports-list.component';
+import {ProfileDetailsComponent} from '../../../components/core/profile/profile-details/profile-details.component'
 import {MockProfileService} from '../../../../data/infrastructure/services/profile/MockProfileService';
 import {ActivatedRoute, Router} from '@angular/router';
 import {Profile} from '../../../../data/domain/models/Profile';
+import {ReportRow} from '../reports/reports.component';
+import {MockUserService} from '../../../../data/infrastructure/services/user/MockUserService';
+import {ReportedUser} from '../../../../data/domain/models/ReportedUser';
+import {ProfileInfo} from '../../../../data/application/services/ProfileService';
 
 @Component({
   selector: 'app-profile',
   imports: [
-    ReportsListComponent
+    ReportsListComponent,
+    ProfileDetailsComponent
   ],
   templateUrl: './profile.component.html',
+  standalone: true,
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent implements OnInit {
   profile: Profile | null = null;
+  reportRows: ReportRow[] = [];
+  reports: ReportedUser[] = [];
 
   constructor(
     private profileService: MockProfileService,
+    private userService: MockUserService,
     private route: ActivatedRoute,
     private router: Router
   ) {}
 
-  ngOnInit() {
-    this.route.queryParams.subscribe(params => {
+  async ngOnInit() {
+    this.route.queryParams.subscribe(async params => {
       const userId = params['id'];
 
       if (userId) {
-        this.profileService.getProfile(userId).then(
-          (profileData) => {
-            this.profile = profileData;
-          },
-          (error) => {
-            console.error('Error al obtener el perfil:', error);
-          }
-        );
+        try {
+          this.profile = await this.profileService.getProfile(userId);
+        } catch (error) {
+          console.error('Error al obtener el perfil:', error);
+        }
       } else {
         console.warn('No se proporcionó un ID de usuario en los parámetros de la URL.');
       }
     });
+
+    try {
+      const reports = await this.userService.getReports();
+      this.reports = reports;
+      console.log('Reports:', reports);
+      this.reportRows = await Promise.all(
+        reports.map(async (report) => {
+          const reportedUserProfile = await this.profileService.getProfileInfo(report.reportedUserId);
+          const reportingUserProfile = await this.profileService.getProfileInfo(report.reportingUserId);
+
+          return {
+            reportedUser: reportedUserProfile,
+            reportingUser: reportingUserProfile,
+            report
+          };
+        })
+      );
+
+      console.log('ReportRows:', this.reportRows);
+    } catch (error) {
+      console.error('Error al obtener los reportes:', error);
+    }
   }
 }
