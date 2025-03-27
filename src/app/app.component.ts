0a8cd7878eaa2b8f7cd2df03325206dec3e52d31
ULPGCJuan <juanantonioher03@gmail.com>
2025-03-27 18:36:59 +0000

    wip login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {NavigationEnd, RouterOutlet} from '@angular/router';
 import {HeaderComponent} from './ui/components/shared/header/header.component';
 import {
   AdministratorSideBarComponent
@@ -9,11 +9,13 @@ import {MockProfileService} from './data/infrastructure/services/profile/MockPro
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import {UserService} from './data/application/services/UserService';
 import {ProfileService} from './data/application/services/ProfileService';
+import { Router } from '@angular/router';
+import {NgIf} from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, AdministratorSideBarComponent, FontAwesomeModule],
+  imports: [RouterOutlet, HeaderComponent, AdministratorSideBarComponent, FontAwesomeModule, NgIf],
   templateUrl: './app.component.html',
   standalone: true,
   providers: [
@@ -22,6 +24,13 @@ import {ProfileService} from './data/application/services/ProfileService';
   ],
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'unimatch-administrator';
+
+  constructor(private router: Router) {}
+
+  isLoginPage(): boolean {
+    return this.router.url === '/login';
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { DashboardComponent } from './ui/screens/core/dashboard/dashboard.compon
 import { ReportsComponent } from './ui/screens/core/reports/reports.component';
 import { UsersComponent } from './ui/screens/core/users/users.component';
 import {ProfileComponent} from './ui/screens/core/profile/profile.component';
+import {LoginComponent} from './ui/screens/auth/login/login.component';
+import {AuthGuard} from './services/auth-service.service';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/admin/dashboard', pathMatch: 'full' },
@@ -14,5 +16,6 @@ export const routes: Routes = [
       { path: 'users', component: UsersComponent }
     ]
   },
-  { path: 'user/:id', component: ProfileComponent },
+  { path: 'user/:id', component: ProfileComponent},
+  { path: 'login', component: LoginComponent }
 ];
